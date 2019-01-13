c4adb401eaa8456cf1009e21907ae06bafeff1a8
soaryang <longfei19880>
2019-01-13 15:44:05 +0800

    commit

--- soar_pay/src/main/java/com/yangtengfei/pay/controller/CardController.java
+++ soar_pay/src/main/java/com/yangtengfei/pay/controller/CardController.java
@@ -114,7 +114,7 @@ public class CardController {
 
         newCardViewList.addAll(cardViewList.stream().filter(item -> item.getIsOpenCard()==1).filter(item -> "蚂蚁花呗".equals(item.getPayType())).collect(Collectors.toList()));
 
-        newCardViewList.addAll(cardViewList.stream().filter(item -> item.getIsOpenCard()==1).filter(item -> "京东白条".equals(item.getPayType())).collect(Collectors.toList()));
+        //newCardViewList.addAll(cardViewList.stream().filter(item -> item.getIsOpenCard()==1).filter(item -> "京东白条".equals(item.getPayType())).collect(Collectors.toList()));
 
         newCardViewList.addAll(cardViewList.stream().filter(item -> item.getIsOpenCard()==1).filter(item -> "信用卡".equals(item.getPayType())).collect(Collectors.toList()));
 
